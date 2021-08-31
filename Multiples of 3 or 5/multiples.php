f7a6ec5480c325dadeef2f73df80813363acee08
deydrums <dagarcia100@gmail.com>
2021-08-31 17:45:15 -0600

    Multiples of 3 or 5

---
+++ Multiples of 3 or 5/multiples.php
@@ -0,0 +1,18 @@
+<!-- /*
+If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. 
+The sum of these multiples is 23.
+
+Find the sum of all the multiples of 3 or 5 below 1000.
+Answer:  233168
+*/ -->
+
+<?php
+
+function multiples($num,$sum = 0) {
+    for ($i=0; $i < $num ; $i++) { 
+        ($i % 3 == 0 || $i % 5 ==0) && $sum = $sum + $i;
+    }
+    return $sum;
+}
+
+var_dump(multiples(1000));
